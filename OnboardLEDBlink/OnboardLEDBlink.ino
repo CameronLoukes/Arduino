bb333981f195d8a787948397880563f716063820
Cameron Loukes <cameron.loukes@gmail.com>
2023-03-01 14:06:27 -0500

    saved changes

--- OnboardLEDBlink/OnboardLEDBlink.ino
+++ OnboardLEDBlink/OnboardLEDBlink.ino
@@ -35,11 +35,11 @@ void setup() {
 // the loop function runs over and over again forever
 void loop() {
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(500);                       // wait for a second
+  delay(500);                       // wait for a half-second
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(250);                       // wait for a second
+  delay(250);                       // wait for a quarter-second
    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(2000);                       // wait for a second
+  delay(2000);                       // wait for a two seconds
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1500);                       // wait for a second
+  delay(1500);                       // wait for a second-and-a-half
 }
